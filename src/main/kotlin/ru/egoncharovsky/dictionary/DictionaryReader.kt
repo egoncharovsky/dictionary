d8be423840e7145d7f390f3919798a53ccc87d25
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2021-03-20 16:26:55 +0300

    clear index feature

--- src/main/kotlin/ru/egoncharovsky/dictionary/DictionaryReader.kt
+++ src/main/kotlin/ru/egoncharovsky/dictionary/DictionaryReader.kt
@@ -4,6 +4,7 @@ import com.twmacinta.util.MD5
 import mu.KotlinLogging
 import java.io.File
 import java.io.RandomAccessFile
+import kotlin.system.measureTimeMillis
 
 class DictionaryReader(dictionaryPath: String, indexPath: String) {
     private object Tag {
@@ -25,48 +26,31 @@ class DictionaryReader(dictionaryPath: String, indexPath: String) {
     private val logger = KotlinLogging.logger { }
 
     fun readArticlePositions(): Map<String, List<Long>> {
-        val dictionaryHash = MD5.asHex(MD5.getHash(dictionaryFile))
-        logger.debug("MD5 dictionary hash: $dictionaryHash")
+        val positions: Map<String, List<Long>>
+        val time = measureTimeMillis {
+            val dictionaryHash = MD5.asHex(MD5.getHash(dictionaryFile))
+            logger.debug("MD5 dictionary hash: $dictionaryHash")
 
-        return if (indexFile.exists()) {
-            val lines = indexFile.readLines()
-            val indexHash = lines[0]
-            if (indexHash == dictionaryHash) {
-                restoreArticlePositions(lines)
+            positions = if (indexFile.exists()) {
+                val lines = indexFile.readLines()
+                val indexHash = lines[0]
+                if (indexHash == dictionaryHash) {
+                    restoreArticlePositions(lines)
+                } else {
+                    logger.debug("Dictionary changed, rescan: dictionary MD5 $dictionaryHash index MD5 $indexHash")
+                    scanArticlePositions().also {
+                        saveArticlePositionsIndex(dictionaryHash, it)
+                    }
+                }
             } else {
-                logger.debug("Dictionary changed, rescan: dictionary MD5 $dictionaryHash index MD5 $indexHash")
+                logger.debug("Index doesn't exist")
                 scanArticlePositions().also {
                     saveArticlePositionsIndex(dictionaryHash, it)
                 }
             }
-        } else {
-            logger.debug("Index doesn't exist")
-            scanArticlePositions().also {
-                saveArticlePositionsIndex(dictionaryHash, it)
-            }
         }
-    }
-
-    private fun restoreArticlePositions(indexLines: List<String>): Map<String, List<Long>> {
-        logger.debug("Restoring index")
-        return indexLines[1].split(indexSeparator).map {
-            val split = it.split(indexKeyValueSeparator)
-            val key = split[0]
-            val position = split[1].toLong()
-            key to position
-        }.groupBy( { it.first }, { it.second } )
-    }
-
-    private fun saveArticlePositionsIndex(hash: String, positions: Map<String, List<Long>>) {
-        logger.debug("Saving index")
-        val index = positions.flatMap { e ->
-            val key = e.key
-            e.value.map { position ->
-                "$key$indexKeyValueSeparator$position"
-            }
-        }.joinToString(indexSeparator)
-
-        indexFile.writeText("$hash\n$index")
+        logger.debug("Article positions read in ${time/1000}.${time%1000} s")
+        return positions
     }
 
     fun scanArticlePositions(): Map<String, List<Long>> = RandomAccessFile(dictionaryFile, "r").use { raf ->
@@ -90,6 +74,30 @@ class DictionaryReader(dictionaryPath: String, indexPath: String) {
         logger.debug("Keys found total: ${keyPositions.size}")
         keyPositions
     }.toMap()
+
+    fun clearIndex() = if (indexFile.exists()) indexFile.delete() else true
+
+    private fun restoreArticlePositions(indexLines: List<String>): Map<String, List<Long>> {
+        logger.debug("Restoring index")
+        return indexLines[1].split(indexSeparator).map {
+            val split = it.split(indexKeyValueSeparator)
+            val key = split[0]
+            val position = split[1].toLong()
+            key to position
+        }.groupBy( { it.first }, { it.second } )
+    }
+
+    private fun saveArticlePositionsIndex(hash: String, positions: Map<String, List<Long>>) {
+        logger.debug("Saving index")
+        val index = positions.flatMap { e ->
+            val key = e.key
+            e.value.map { position ->
+                "$key$indexKeyValueSeparator$position"
+            }
+        }.joinToString(indexSeparator)
+
+        indexFile.writeText("$hash\n$index")
+    }
 
     private fun parseKey(articleLine: String): String? = keyRegex.find(articleLine)?.groups?.get(1)?.value
 }
