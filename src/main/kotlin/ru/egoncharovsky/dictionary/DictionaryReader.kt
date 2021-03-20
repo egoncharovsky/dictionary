8311e78f9047dd3ab17386114ad822dced0ff350
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2021-03-20 16:10:48 +0300

    saving index for articles

--- src/main/kotlin/ru/egoncharovsky/dictionary/DictionaryReader.kt
+++ src/main/kotlin/ru/egoncharovsky/dictionary/DictionaryReader.kt
@@ -5,10 +5,7 @@ import mu.KotlinLogging
 import java.io.File
 import java.io.RandomAccessFile
 
-class DictionaryReader(
-    private val dictionaryPath: String,
-    private val indexPath: String
-) {
+class DictionaryReader(dictionaryPath: String, indexPath: String) {
     private object Tag {
         const val article = "<ar>"
 
@@ -18,36 +15,81 @@ class DictionaryReader(
     }
 
     private val dictionaryFile = File(dictionaryPath)
-    private val indexFile = File(dictionaryPath)
+    private val indexFile = File(indexPath)
 
     private val keyRegex = "${Tag.key}(.*)${Tag.closing(Tag.key)}".toRegex()
 
-    private val logger = KotlinLogging.logger {  }
+    private val indexSeparator = ","
+    private val indexKeyValueSeparator = ":"
+
+    private val logger = KotlinLogging.logger { }
 
     fun readArticlePositions(): Map<String, List<Long>> {
-        val hash = MD5.asHex(MD5.getHash(dictionaryFile))
-        logger.debug("MD5 hash: $hash")
+        val dictionaryHash = MD5.asHex(MD5.getHash(dictionaryFile))
+        logger.debug("MD5 dictionary hash: $dictionaryHash")
 
-        return RandomAccessFile(dictionaryFile, "r").use { raf ->
-            val keyPositions = mutableMapOf<String, List<Long>>()
-            while (raf.filePointer < 50000) {
-                val lineStart = raf.filePointer
-                raf.readLine(charset("UTF-8"))?.let { line ->
-                    if (line.startsWith(Tag.article)) {
-
-                        val key = parseKey(line) ?: throw KeyNotFound(lineStart, line)
-
-                        keyPositions[key] = keyPositions[key]?.plus(lineStart) ?: run {
-                            listOf(lineStart)
-                        }
-
-                    }
+        return if (indexFile.exists()) {
+            val lines = indexFile.readLines()
+            val indexHash = lines[0]
+            if (indexHash == dictionaryHash) {
+                restoreArticlePositions(lines)
+            } else {
+                logger.debug("Dictionary changed, rescan: dictionary MD5 $dictionaryHash index MD5 $indexHash")
+                scanArticlePositions().also {
+                    saveArticlePositionsIndex(dictionaryHash, it)
                 }
             }
-            logger.debug("Keys found total: ${keyPositions.size}")
-            keyPositions
-        }.toMap()
+        } else {
+            logger.debug("Index doesn't exist")
+            scanArticlePositions().also {
+                saveArticlePositionsIndex(dictionaryHash, it)
+            }
+        }
     }
+
+    private fun restoreArticlePositions(indexLines: List<String>): Map<String, List<Long>> {
+        logger.debug("Restoring index")
+        return indexLines[1].split(indexSeparator).map {
+            val split = it.split(indexKeyValueSeparator)
+            val key = split[0]
+            val position = split[1].toLong()
+            key to position
+        }.groupBy( { it.first }, { it.second } )
+    }
+
+    private fun saveArticlePositionsIndex(hash: String, positions: Map<String, List<Long>>) {
+        logger.debug("Saving index")
+        val index = positions.flatMap { e ->
+            val key = e.key
+            e.value.map { position ->
+                "$key$indexKeyValueSeparator$position"
+            }
+        }.joinToString(indexSeparator)
+
+        indexFile.writeText("$hash\n$index")
+    }
+
+    fun scanArticlePositions(): Map<String, List<Long>> = RandomAccessFile(dictionaryFile, "r").use { raf ->
+        logger.debug("Scanning dictionary...")
+
+        val keyPositions = mutableMapOf<String, List<Long>>()
+        while (raf.filePointer < raf.length()) {
+            val lineStart = raf.filePointer
+            raf.readLine(charset("UTF-8"))?.let { line ->
+                if (line.startsWith(Tag.article)) {
+
+                    val key = parseKey(line) ?: throw KeyNotFound(lineStart, line)
+
+                    keyPositions[key] = keyPositions[key]?.plus(lineStart) ?: run {
+                        listOf(lineStart)
+                    }
+
+                }
+            }
+        }
+        logger.debug("Keys found total: ${keyPositions.size}")
+        keyPositions
+    }.toMap()
 
     private fun parseKey(articleLine: String): String? = keyRegex.find(articleLine)?.groups?.get(1)?.value
 }
